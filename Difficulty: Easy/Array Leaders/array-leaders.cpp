66b6697d4b7af34ab3e53894e1e449867e10b262
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-12 13:48:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.cpp
@@ -0,0 +1,18 @@
+
+
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // code here
+        vector<int>res;
+        int n=arr.size();
+        res.push_back(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=res.back()){
+                res.push_back(arr[i]);
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
