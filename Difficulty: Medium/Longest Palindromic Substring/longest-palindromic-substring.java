f22b9237ca5183eb806676e01b347d954652ec1e
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-14 15:28:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Substring/longest-palindromic-substring.java
@@ -0,0 +1,22 @@
+class Solution {
+	public String getLongestPal(String s) {
+		// code here
+		int n = s.length();
+		boolean [][] dp = new boolean[n][n];
+		int start = 0;
+		int maxlen = 1;
+		
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j<n; j++) {
+				if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+					dp[i][j] = true;
+					if (j - i + 1 > maxlen || (j - i + 1 == maxlen && i<start)) {
+						maxlen = j - i + 1;
+						start = i;
+					}
+				}
+			}
+		}
+		return s.substring(start, start + maxlen);
+	}
+}
