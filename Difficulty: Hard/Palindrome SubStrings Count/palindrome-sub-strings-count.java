cea370e30f0356133b128dd33edbebbf8fd52ad7
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-14 15:55:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Palindrome SubStrings Count/palindrome-sub-strings-count.java
@@ -0,0 +1,21 @@
+class Solution {
+	public int countPS(String s) {
+		// code here
+		int n = s.length();
+		boolean [][] dp = new boolean[n][n];
+		
+		int count = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j<n; j++) {
+				if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])) {
+					dp[i][j] = true;
+					
+					if(j-i+1 >=2){
+					    count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
+}
