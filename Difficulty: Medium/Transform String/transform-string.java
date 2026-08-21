bc43af92faaee14b665fb2841555f26357369063
Souradip <145707173+souradip23@users.noreply.github.com>
2026-08-21 14:04:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Transform String/transform-string.java
+++ Difficulty: Medium/Transform String/transform-string.java
@@ -1,44 +1,35 @@
 class Solution {
+
     int transform(String s1, String s2) {
-        // code here
-        if (s1.length() != s2.length()) {
-			return - 1;
-		}
+        int n = s1.length();
+        if (n != s2.length()) {
+            return -1;
+        }
 
-		HashMap<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[256];
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i)]++;
+            freq[s2.charAt(i)]--;
+        }
+        for (int f : freq) {
+            if (f != 0) {
+                return -1;
+            }
+        }
 
-		for (char c : s1.toCharArray()) {
-			map.put(c, map.getOrDefault(c, 0) + 1);
-		}
-
-		for (char c : s2.toCharArray()) {
-			if (!map.containsKey(c)) {
-				return - 1;
-			}
-
-			map.put(c, map.get(c) - 1);
-
-			if (map.get(c) < 0) {
-				return - 1;
-			}
-		}
-		int n = s1.length() - 1;
-		int m = s2.length() - 1;
-		int count = 0;
-		while (n >= 0 && m >= 0) {
-			char one = s1.charAt(n);
-			char two = s2.charAt(m);
-			if (one == two) {
-				n--;
-				m--;
-			} else {
-				while ( n >= 0 && s1.charAt(n) != s2.charAt(m) ) {
-					count++;
-					n--;
-				}
-			}
-		}
-
-		return count;
+        int i = n - 1;
+        int j = n - 1;
+        int ops = 0;
+        while (i >= 0) {
+            while (i >= 0 && s1.charAt(i) != s2.charAt(j)) {
+                ops++;
+                i--;
+            }
+            if (i >= 0) {
+                i--;
+                j--;
+            }
+        }
+        return ops;
     }
 }
